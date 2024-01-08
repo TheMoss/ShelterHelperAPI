adedc87aa027d6552af8b3ec7bd1443bfd80a362
TheMoss <milena.k98@gmail.com>
2024-01-08 16:27:44 +0100

    Initial project version

---
+++ Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelperAPI.Models
+{
+	public class Animal
+	{		
+			public int Id { get; set; }
+			public string Species { get; set; }
+			public string Name { get; set; }
+			public string Sex { get; set; }
+			public int Weight { get; set; }
+			[Display(Name = "Admission day")]
+			[DataType(DataType.Date)]
+			public DateTime AdmissionDay { get; set; }
+			[Display(Name = "Adoption day")]
+			[DataType(DataType.Date)]
+			public DateTime AdoptionDay { get; set; }
+			public string Health { get; set; }
+			[Display(Name = "Employee ID")]
+			public int EmployeeId { get; set; }
+		
+	}
+}
---
+++ Models/ShelterContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace ShelterHelperAPI.Models
+{
+    public class ShelterContext : DbContext
+    {
+		public DbSet<Animal> AnimalsDb { get; set; }		
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var configuration = new ConfigurationBuilder()
+			.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+			
+			optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+		public ShelterContext(DbContextOptions<ShelterContext> options) : base(options) {
+			
+		}
+	}
+}
