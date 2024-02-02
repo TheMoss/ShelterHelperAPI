8565e843a5faf60a579e6ef6107741be5ef405f3
TheMoss <milena.k98@gmail.com>
2024-02-03 00:40:54 +0100

    refactor: remove unnecessary usings, switch datatype from DateTime to DateOnly to better fit the purpose of keeping track of dates

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -1,108 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShelterHelperAPI.Models;
 
 namespace ShelterHelperAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AnimalsController : ControllerBase
-    {
-        private readonly ShelterContext _context;
-        
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AnimalsController : ControllerBase
+	{
+		private readonly ShelterContext _context;
 
-        public AnimalsController(ShelterContext context)
-        {
-            _context = context;
-        }
 
-        // GET: api/Animals
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimalsDb()
-        {
-            return await _context.AnimalsDb.ToListAsync();
-        }
+		public AnimalsController(ShelterContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Animals/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Animal>> GetAnimal(int id)
-        {
-            var animal = await _context.AnimalsDb.FindAsync(id);
+		// GET: api/Animals
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Animal>>> GetAnimalsDb()
+		{
+			return await _context.AnimalsDb.ToListAsync();
+		}
 
-            if (animal == null)
-            {
-                return NotFound();
-            }
+		// GET: api/Animals/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Animal>> GetAnimal(int id)
+		{
+			var animal = await _context.AnimalsDb.FindAsync(id);
 
-            return animal;
-        }
+			if (animal == null)
+			{
+				return NotFound();
+			}
 
-        // PUT: api/Animals/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAnimal(int id, Animal animal)
-        {
-            if (id != animal.Id)
-            {
-                return BadRequest();
-            }
+			return animal;
+		}
 
-            _context.Entry(animal).State = EntityState.Modified;
+		// PUT: api/Animals/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutAnimal(int id, Animal animal)
+		{
+			if (id != animal.Id)
+			{
+				return BadRequest();
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AnimalExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			_context.Entry(animal).State = EntityState.Modified;
 
-            return NoContent();
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!AnimalExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        // POST: api/Animals
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
-        {
-            _context.AnimalsDb.Add(animal);
-            await _context.SaveChangesAsync();
+			return NoContent();
+		}
 
-            return CreatedAtAction("GetAnimal", new { id = animal.Id }, animal);
-        }
+		// POST: api/Animals
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
+		{
+			_context.AnimalsDb.Add(animal);
+			await _context.SaveChangesAsync();
 
-        // DELETE: api/Animals/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAnimal(int id)
-        {
-            var animal = await _context.AnimalsDb.FindAsync(id);
-            if (animal == null)
-            {
-                return NotFound();
-            }
+			return CreatedAtAction("GetAnimal", new { id = animal.Id }, animal);
+		}
 
-            _context.AnimalsDb.Remove(animal);
-            await _context.SaveChangesAsync();
+		// DELETE: api/Animals/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteAnimal(int id)
+		{
+			var animal = await _context.AnimalsDb.FindAsync(id);
+			if (animal == null)
+			{
+				return NotFound();
+			}
 
-            return NoContent();
-        }
+			_context.AnimalsDb.Remove(animal);
+			await _context.SaveChangesAsync();
 
-        private bool AnimalExists(int id)
-        {
-            return _context.AnimalsDb.Any(e => e.Id == id);
-        }
-    }
+			return NoContent();
+		}
+
+		private bool AnimalExists(int id)
+		{
+			return _context.AnimalsDb.Any(e => e.Id == id);
+		}
+	}
 }
--- Models/Animal.cs
+++ Models/Animal.cs
@@ -4,17 +4,17 @@ namespace ShelterHelperAPI.Models
 {
 	public class Animal
 	{		
-			public int Id { get; set; }
+			public int? Id { get; set; }
 			public string Species { get; set; }
 			public string Name { get; set; }
 			public string Sex { get; set; }
 			public int Weight { get; set; }
 			[Display(Name = "Admission day")]
-			[DataType(DataType.Date)]
-			public DateTime AdmissionDay { get; set; }
-			[Display(Name = "Adoption day")]
-			[DataType(DataType.Date)]
-			public DateTime AdoptionDay { get; set; }
+			
+			public DateOnly AdmissionDay { get; set; }
+		[Display(Name = "Adoption day")]
+
+		public DateOnly? AdoptionDay { get; set; }
 			public string Health { get; set; }
 			[Display(Name = "Employee ID")]
 			public int EmployeeId { get; set; }
