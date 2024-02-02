5b0b2e691e3363db95ed5a7d45dee2d9d8598531
TheMoss <milena.k98@gmail.com>
2024-02-03 00:46:43 +0100

    feat: add a controller for managing the section of a database dedicated to species and their attributes

---
+++ Controllers/SpeciesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+using ShelterHelperAPI.Models;
+
+namespace ShelterHelperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeciesController : ControllerBase
+    {
+        private readonly ShelterContext _context;
+
+        public SpeciesController(ShelterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Species
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Species>>> GetSpeciesDb()
+        {
+            
+           return await _context.Species.ToListAsync();			          
+		}
+
+        // GET: api/Species/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Species>> GetSpecies(string id)
+        {
+            var species = await _context.Species.FindAsync(id);
+
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            return species;
+        }
+
+        // PUT: api/Species/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSpecies(int id, Species species)
+        {
+            if (id != species.SpeciesId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(species).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpeciesExists(id.ToString()))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Species
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Species>> PostSpecies(Species species)
+        {
+            _context.Species.Add(species);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SpeciesExists(species.SpeciesId.ToString()))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSpecies", new { id = species.SpeciesId }, species);
+        }
+
+        // DELETE: api/Species/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSpecies(int id)
+        {
+            var species = await _context.Species.FindAsync(id);
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            _context.Species.Remove(species);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SpeciesExists(string id)
+        {
+            return _context.Species.Any(e => e.SpeciesId.ToString() == id);
+        }
+    }
+}
