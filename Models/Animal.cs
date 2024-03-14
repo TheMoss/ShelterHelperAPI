b8a3243e5f221e0a7c3f54254e1516b9dd1cd427
TheMoss <milena.k98@gmail.com>
2024-03-14 17:37:16 +0100

    refactor: remove unnecessary validation and create a referfence to Species model.

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -4,20 +4,15 @@ namespace ShelterHelperAPI.Models
 {
 	public class Animal
 	{		
-			public int? Id { get; set; }
-			public string Species { get; set; }
-			public string Name { get; set; }
-			public string Sex { get; set; }
-			public int Weight { get; set; }
-			[Display(Name = "Admission day")]
-			
-			public DateOnly AdmissionDay { get; set; }
-		[Display(Name = "Adoption day")]
-
+		public int? Id { get; set; }
+		public int SpeciesId { get; set; }
+		public virtual Species? Species { get; set; }
+		public string Name { get; set; }
+		public string Sex { get; set; }
+		public int Weight { get; set; }			
+		public DateOnly AdmissionDay { get; set; }
 		public DateOnly? AdoptionDay { get; set; }
-			public string Health { get; set; }
-			[Display(Name = "Employee ID")]
-			public int EmployeeId { get; set; }
-		
+		public string Health { get; set; }
+		public int EmployeeId { get; set; }		
 	}
 }
