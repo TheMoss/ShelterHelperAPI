ef93acf5debe515db1cfdcbf9a8944731def3d22
TheMoss <milena.k98@gmail.com>
2024-05-24 17:10:26 +0200

    style: rename StorageController to ResourcesController, new name better reflects its function.

--- Controllers/ResourcesController.cs
+++ Controllers/ResourcesController.cs
@@ -7,18 +7,18 @@ using ShelterHelperAPI.Models;
 namespace ShelterHelperAPI.Controllers
 {
 	
-	[Route("api/[controller]")]
+	[Route("api/resources")]
 	[ApiController]
-	public class StorageController : ControllerBase
+	public class ResourcesController : ControllerBase
 	{
 		private readonly ShelterContext _context;
 
-		public StorageController(ShelterContext context)
+		public ResourcesController(ShelterContext context)
 		{
 			_context = context;
 		}
 
-		//GET: api/Storage/Get
+		//GET: api/resources
 		[HttpGet]
 		public async Task<ActionResult<StorageDto>> Get()
 		{
@@ -32,63 +32,101 @@ namespace ShelterHelperAPI.Controllers
 			return attributesDto;
 		}
 
-		// GET api/<StorageController>/5
-		[HttpGet("{id}")]
-		public string Get(int id)
+		#region Diets
+
+		//GET api/resources/diets/1
+		[HttpGet("diets/{id}")]
+		public async Task<ActionResult<Diet>> GetDiet(int id)
 		{
-			return "value";
+			var diet = await _context.Diet.FindAsync(id);
+			if (diet == null)
+			{
+				return NotFound();
+			}
+			return diet;
 		}
 
-		// POST api/<StorageController>/Diet
+		// POST api/resources/diet
 		[HttpPost]
-		[Route("Diet")]
+		[Route("diets")]
 		public async Task<ActionResult<Diet>> PostNewDiet(Diet diet)
 		{
 			_context.Diet.Add(diet);
 			await _context.SaveChangesAsync();
 			return CreatedAtAction(nameof(Get), new {id = diet.DietId}, diet);
 		}
+		#endregion
 
-		// POST api/<StorageController>/Bedding
+		#region Beddings
+		//GET api/resources/beddings/1
+		[HttpGet("beddings/{id}")]
+		public async Task<ActionResult<Bedding>> GetBedding(int id)
+		{
+			var bedding = await _context.Bedding.FindAsync(id);
+			if (bedding == null)
+			{
+				return NotFound();
+			}
+			return bedding;
+		}
+		// POST api/resources/beddings
 		[HttpPost]
-		[Route("Bedding")]
+		[Route("beddings")]
 		public async Task<ActionResult<Bedding>> PostNewBedding(Bedding bedding)
 		{
 			_context.Bedding.Add(bedding);
 			await _context.SaveChangesAsync();
 			return CreatedAtAction(nameof(Get), new { id = bedding.BeddingId }, bedding);
 		}
+		#endregion
 
-		// POST api/<StorageController>/Toy
+		#region Toys
+		//GET api/resources/toys/1
+		[HttpGet("toys/{id}")]
+		public async Task<ActionResult<Toy>> GetToy(int id)
+		{
+			var toy = await _context.Toy.FindAsync(id);
+			if (toy == null)
+			{
+				return NotFound();
+			}
+			return toy;
+		}
+
+
+		// POST api/resources/toys
 		[HttpPost]
-		[Route("Toy")]
+		[Route("toys")]
 		public async Task<ActionResult<Toy>> PostNewToy(Toy toy)
 		{
 			_context.Toy.Add(toy);
 			await _context.SaveChangesAsync();
 			return CreatedAtAction(nameof(Get), new { id = toy.ToyId}, toy);
 		}
+		#endregion
 
-		// POST api/<StorageController>/Accessory
+		#region Accessories
+		//GET api/resources/accessories/1
+		[HttpGet("accessories/{id}")]
+		public async Task<ActionResult<Accessory>> GetAccessory(int id)
+		{
+			var accessory = await _context.Accessory.FindAsync(id);
+			if (accessory == null)
+			{
+				return NotFound();
+			}
+			return accessory;
+		}
+
+		// POST api/resources/accessories
 		[HttpPost]
-		[Route("Accessory")]
+		[Route("accessories")]
 		public async Task<ActionResult<Accessory>> PostNewAccessory(Accessory accessory)
 		{
 			_context.Accessory.Add(accessory);
 			await _context.SaveChangesAsync();
 			return CreatedAtAction(nameof(Get), new { id = accessory.AccessoryId}, accessory);
 		}
-
-		// PUT api/<StorageController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
-
-		// DELETE api/<StorageController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
+		#endregion
 	}
 }
