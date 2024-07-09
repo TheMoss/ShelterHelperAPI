ea505b91fa7f6ecf29c2749cf2e883d20d20ef3d
TheMoss <milena.k98@gmail.com>
2024-07-09 23:06:53 +0200

    feat: add a model representing an Employee class

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelperAPI.Models
+{ 
+	public class Employee
+	{
+		[Key]
+		public int EmployeeId { get; set; }		
+		public int EmployeePersonalId{ get; set; }
+		public string EmployeeName { get; set; }
+
+	}
+}
