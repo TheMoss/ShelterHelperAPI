34105ce8f725825440b0e04bcfc522c8ee165a08
TheMoss <milena.k98@gmail.com>
2024-07-16 18:09:15 +0200

    refactor: declare SpeciesId, Sex and Health properties as nullable
    These properties shouldn't be necessary for PATCH request

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -6,15 +6,15 @@ namespace ShelterHelperAPI.Models
 	public class Animal
 	{		
 		public int? Id { get; set; }
-		public int SpeciesId { get; set; }
+		public int? SpeciesId { get; set; }
 		public virtual Species? Species { get; set; }
 		public int ChipNumber { get; set; }
 		public string Name { get; set; }
-		public string Sex { get; set; }
+		public string? Sex { get; set; }
 		public int Weight { get; set; }			
 		public DateOnly AdmissionDay { get; set; }
 		public DateOnly? AdoptionDay { get; set; }
-		public string Health { get; set; }
+		public string? Health { get; set; }
 		public virtual Employee? Employee { get; set; }	
 		public int EmployeeId { get; set; }
 
