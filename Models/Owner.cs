b4ff8a046f4e5764a7f9974e83b93b45705f1425
TheMoss <milena.k98@gmail.com>
2024-07-18 22:43:53 +0200

    feat: add Owner model

---
+++ Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelperAPI.Models
+{
+	public class Owner
+	{
+		[Key]
+		public int? OwnerId { get; set; }
+		public string OwnerName { get; set; }
+		public string Address { get; set; }
+		public string? Email { get; set; }
+	}
+}
