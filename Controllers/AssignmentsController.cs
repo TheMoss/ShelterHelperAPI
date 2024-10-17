5c380d6b03787e1ecbcff2c8ca745c3b01e15c2e
TheMoss <milena.k98@gmail.com>
2024-10-17 18:16:29 +0200

    feat: add assignment model and controller

---
+++ Controllers/AssignmentsController.cs
@@ -0,0 +1,102 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShelterHelperAPI.Models;
+
+namespace ShelterHelperAPI.Controllers
+{
+    [Route("api/assignments")]
+    [ApiController]
+    public class AssignmentsController : ControllerBase
+    {
+        private readonly ShelterContext _context;
+
+        public AssignmentsController(ShelterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/assignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Assignment>>> GetAssignments()
+        {
+            return await _context.Assignment.ToListAsync();
+        }
+
+        // GET: api/assignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Assignment>> GetAssignment(int? id)
+        {
+            var assignment = await _context.Assignment.FindAsync(id);
+
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return assignment;
+        }
+
+        // PUT: api/assignments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAssignment(int? id, Assignment assignment)
+        {
+            if (id != assignment.AssignmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(assignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/assignments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Assignment>> PostAssignment(Assignment assignment)
+        {
+            _context.Assignment.Add(assignment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAssignment", new { id = assignment.AssignmentId }, assignment);
+        }
+
+        // DELETE: api/Assignments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAssignment(int? id)
+        {
+            var assignment = await _context.Assignment.FindAsync(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Assignment.Remove(assignment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AssignmentExists(int? id)
+        {
+            return _context.Assignment.Any(e => e.AssignmentId == id);
+        }
+    }
+}
